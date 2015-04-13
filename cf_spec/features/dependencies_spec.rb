e738de1622d217259d1149c9a9813aafa5e46228
JT Archie <jtarchie@gmail.com>
2015-04-13 11:31:01 -0400

    WIP: testing with docker on feature test
    Signed-off-by: Sai To Yeung <saitoyeung@gmail.com>

--- cf_spec/features/dependencies_spec.rb
+++ cf_spec/features/dependencies_spec.rb
@@ -1,23 +1,32 @@
 require 'spec_helper'
 require 'support/python_environment'
 
+
 describe 'dependencies in the manifest' do
-  before(:all) { @env = PythonEnvironment.new }
+  before do
+    @env = DockerEnvironment.new
+    @env.map('~/workspace/python-buildpack', '/buildpack')
+    @env.map('~/workspace/python-buildpack/cf_spec/fixtures/features', '/src')
+    @env.exec(cmd: 'cp', args: ['-a', '/src/.', '/build'])
+    @env.exec(cmd: 'mkdir', args: ['-p', '/app'])
+    @env.exec(cmd: '/buildpack/bin/compile', args: ['/build', '/tmp'], working_dir: '/buildpack')
+  end
+
+  after { @env.cleanup! }
 
   describe '#libffi' do
     it 'can integrate with C-header files' do
-      @env.execute(file: 'libffi.py') do |output|
+      @env.python(file: '/build/libffi.py') do |output|
         expect(output).to eq "hi there, world!\n"
       end
     end
   end
 
   describe '#libmemcache' do
-    it 'can interact with memcache server' do
-      @env.execute(file: 'libmemcache.py') do |output|
+    it 'cannot connect with the memcache server' do
+      @env.python(file: '/build/libmemcache.py') do |output|
         expect(output).to eq "Could not connect\n"
       end
     end
   end
 end
-
--- cf_spec/support/python_environment.rb
+++ cf_spec/support/python_environment.rb
@@ -1,18 +1,86 @@
-class PythonEnvironment
-  def app
-    @app ||= Machete.deploy_app('flask_web_app')
+class DockerEnvironment
+  def map(source_dir, target_dir)
+    @volumes ||= []
+    @volumes << "-v #{source_dir}:#{target_dir}:ro"
   end
 
-  def execute(file: nil, code: nil, &block)
-    if file
-      file_path = File.join(Dir.pwd, 'cf_spec', 'fixtures', 'features', file)
-      code = File.read(file_path)
-    end
+  def exec(cmd:, args: [], working_dir: nil)
+    `docker run #{@volumes.join(' ')} --name #{name} -d -it cloudfoundry/lucid64 bash`
+    `docker exec #{name} #{cmd} #{args.join ' '}`
+  end
 
-    code.gsub!(/^#{code.scan(/^\s*/).min_by{|l|l.length}}/, "")
+  def python(file:, &block)
+    exec(cmd: 'source', args: ['/build/**/*.sh'])
+    output = exec(
+      cmd: '/build/.heroku/python/bin/python',
+      args: [file]
+    )
+    block.call(output)
+  end
 
-    app_url = Machete::CF::CLI.url_for_app(app)
-    response = HTTParty.post("http://#{app_url}/execute", body: {code: code})
-    yield(response.body)
+  def cleanup!
+    `docker rm -f #{name}`
+  end
+
+  private
+
+  def name
+    @name ||= "docker-env-#{Time.now.to_i}"
+  end
+end
+
+
+# class PythonEnvironment
+  # def app
+    # @app ||= Machete.deploy_app('flask_web_app')
+  # end
+
+  # def execute(file: nil, code: nil, &block)
+    # if file
+      # file_path = File.join(Dir.pwd, 'cf_spec', 'fixtures', 'features', file)
+      # code = File.read(file_path)
+    # end
+
+    # code.gsub!(/^#{code.scan(/^\s*/).min_by{|l|l.length}}/, "")
+
+    # app_url = Machete::CF::CLI.url_for_app(app)
+    # response = HTTParty.post("http://#{app_url}/execute", body: {code: code})
+    # yield(response.body)
+  # end
+# end
+
+require 'shellwords'
+
+class PythonEnvironment
+  def initialize
+    raise "dockery-build missing" unless File.exist?(dockery_build_dir)
+  end
+
+  def execute(file: nil, &block)
+    command = [
+      "#{dockery_build_dir}/bin/deploy",
+      '-b', buildpack_dir,
+      '-a', app_dir,
+      '-c', "python #{file}"
+    ].shelljoin
+
+    puts "Running command: #{command}"
+
+    output = `#{command}`
+    yield(output)
+  end
+
+  private
+
+  def dockery_build_dir
+    File.expand_path('~/workspace/dockery-build')
+  end
+
+  def buildpack_dir
+    File.expand_path('~/workspace/python-buildpack')
+  end
+
+  def app_dir
+    File.join(buildpack_dir, 'cf_spec', 'fixtures', 'features')
   end
 end
